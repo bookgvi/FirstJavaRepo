fc29d384f2bf992bb52861732a02589d2851702c
book <bookgvi@gmail.com>
2020-02-14 21:44:49 +0300

    master: empty

--- src/Interfaces/ITwoNumbers.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-public interface ITwoNumbers {
-    int x = 0;
-    int y = 0;
-
-    int getSum();
-    void setXY(int x, int y);
-}
--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,5 @@
-import Interfaces.ITwoNumbers;
-
 public class Program {
     public static void main(String args[]) {
-        final int X = 5, Y = 5;
-
         System.out.println("Hello, world: Java");
-        ITwoNumbers obj = new SumTwoNumbers(2, 2);
-        System.out.println(obj.getSum());
-        obj.setXY(X, Y);
-        System.out.println(obj.getSum());
     }
 }
