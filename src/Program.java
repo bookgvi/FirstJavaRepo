8d6cfb7df37699f98f18c9c107c18dcc10249939
book <bookgvi@gmail.com>
2020-02-14 21:54:19 +0300

    Input data via java.util.Scanner

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Program {
     public static void main(String args[]) {
         System.out.println("Hello, world: Java");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = in.nextLine();
+        System.out.print("Age: ");
+        int age = in.nextInt();
+        System.out.print("Height: ");
+        float height = in.nextFloat();
+        System.out.print("Male: ");
+        boolean isMale = in.nextBoolean();
+
+        System.out.printf("%s, %d years old. Height - $.2f, Male - %b", name, age, height, isMale);
+        in.close();
     }
 }
