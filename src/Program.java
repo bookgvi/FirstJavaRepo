5f19a07710c12a0d78473a7e67fb096ef3c0a542
book <bookgvi@gmail.com>
2020-02-14 22:16:43 +0300

    Some array

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,16 @@
 public class Program {
     public static void main(String args[]) {
         System.out.println("Hello, world: Java");
+        int a = 0b1111_0000;
+        System.out.println(~a);
+
+        int arr[], b = 1;
+        arr = new int[4];
+        for (int i = 0, length = arr.length; i < length; i++){
+            arr[i] = b++;
+        }
+        for (int i = 0, length = arr.length; i < length; i++){
+            System.out.println(arr[i]);
+        }
     }
 }
