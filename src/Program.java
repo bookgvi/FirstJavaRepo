819f9c1cf237ac5e64001a51e2e3e55393af6e57
book <bookgvi@gmail.com>
2020-02-24 13:53:58 +0300

    delete unnesesary code

--- src/Program.java
+++ src/Program.java
@@ -1,20 +1,10 @@
 import Factory.MergeSortFactory;
 import Factory.QuickSortFactory;
-import Interfaces.Sort;
 import Utils.DisplayArray;
 import Utils.PrepareUnsortedArray;
 
 public class Program {
     public static void main(String args[]) {
-//        System.out.println("Hello, world: Java");
-//
-//        int[] arr = new int[]{1, 2, 3, 4};
-//        try {
-//            arr[5] = 55;
-//            System.out.println("Program has terminated normally");
-//        } catch (Exception err) {
-//            err.printStackTrace();
-//        }
         int ARRAY_SIZE = 50;
         PrepareUnsortedArray pua = new PrepareUnsortedArray(ARRAY_SIZE);
         int[] arr = pua.invoke();
